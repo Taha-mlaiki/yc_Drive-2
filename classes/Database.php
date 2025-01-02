68a547b7283e3b460261ff8753519d3631768835
taha <mlaikitaha29@gmail.com>
2025-01-02 12:24:10 +0100

    #29 Create the database class

--- classes/Database.php
+++ classes/Database.php
@@ -6,24 +6,30 @@ class Database
     private $dbName = "driveloc";
     private $username = "root";
     private $password = "";
-    private $pdo;
+    private static $instance = null;
+    private $connection ;
 
-    public function __construct()
+    private function __construct()
     {
-        // Try to connect to the database
         try {
             $dsn = "mysql:host={$this->host};dbname={$this->dbName}";
-            $this->pdo = new PDO($dsn, $this->username, $this->password);
+            $this->connection = new PDO($dsn, $this->username, $this->password);
 
-            // Best practice because of it help catching errrors
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
         }
     }
 
-    public function prepare($sql)
-    {
-        return $this->pdo->prepare($sql);
+    static function getInstance (){
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
+
+    public function getConnection (){
+        return $this->connection;
+    }
+
 }
