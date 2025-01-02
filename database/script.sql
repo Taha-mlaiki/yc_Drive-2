e1517e3b1835dcb82cae4533ff1cd40326d4cca8
taha <mlaikitaha29@gmail.com>
2025-01-02 10:37:26 +0100

    #27 Create the database and save the script

---
+++ database/script.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS driveloc;
+CREATE DATABASE driveloc;
+
+USE driveloc;
+
+CREATE TABLE role (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255)
+);
+
+CREATE TABLE users (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	username VARCHAR(255),
+    email VARCHAR(255),
+    password TEXT,
+	role_id INT ,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+CREATE TABLE category (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255)
+);
+
+CREATE TABLE vehicle (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255),
+    description TEXT ,
+    modal INT,
+    price INT,
+    available BOOLEAN ,
+    category_id INT ,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE review (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	star INT,
+    user_id INT ,
+	vehicle_id INT ,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(id)
+);
+
+CREATE TABLE reservation (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	date DATE,
+    place VARCHAR(255),
+    user_id INT ,
+	vehicle_id INT ,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(id)
+)
